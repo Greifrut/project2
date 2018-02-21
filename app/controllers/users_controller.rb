53841466d7c910bffff1578829037080bfee7694
Greifrut <artur.bunko@gmail.com>
2018-02-21 14:26:41 +0300

    страница со всеми пользователями

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
@@ -46,7 +50,7 @@ class UsersController < ApplicationController
     def signed_in_user
       unless signed_in?
         store_location
-        redirect_to signin_url, notice: "Please sign in." 
+        redirect_to signin_url, notice: "Please sign in."
       end
     end
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,22 @@ describe "User pages" do
 
   subject {page}
 
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: 'Bob', email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit user_path
+    end
 
+    it {should have_title('All user')}
+    it {should have_content('All user')}
+
+    it "should list each user" do
+      User.all.each do |user|
+        expect(page).to have_selector('li', text: user.name)
+    end
+  end
 
   describe "signup page" do
     before { visit signup_path}
@@ -71,57 +86,6 @@ describe "User pages" do
     end
   end
 
-  describe "autorization", type: :request do
-    describe "for non-signed-in users" do
-      let(:user) {FactoryGirl.create(:user)}
-
-      describe "when attemptinng to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
-
-        describe "after signing in" do
-
-          it "should render the desired protected page" do
-            expect(page).to have_title(user.name)
-          end
-        end
-      end
-
-      describe "in the Users controller" do
-
-        describe "visiting to edit pages" do
-          before {visit edit_user_path(user)}
-          it {should have_title('Sign in')}
-        end
-
-        describe "submitting to the update action" do
-          before {patch user_path(user)}
-          specify {expect(response).to redirect_to(signin_path)}
-        end
-      end
-    end
-
-    describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user, no_capybara: true }
-
-      describe "submitting a GET request to the Users#edit action" do
-        before { get edit_user_path(wrong_user) }
-        specify { expect(response.body).not_to match(full_title('Edit user')) }
-        specify { expect(response).to redirect_to(root_path) }
-      end
-
-      describe "submitting a PATCH request to the Users#update action" do
-        before { patch user_path(wrong_user) }
-        specify { expect(response).to redirect_to(root_path) }
-      end
-    end
-  end
 
   describe "Profile page " do
     let(:user) {FactoryGirl.create(:user)}
