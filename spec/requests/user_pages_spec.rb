c5b14301e096d4fe615d7d6c1d0cf95ddef3aa05
Greifrut <artur.bunko@gmail.com>
2018-02-21 15:03:31 +0300

    пагинация

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,15 +9,23 @@ describe "User pages" do
       sign_in FactoryGirl.create(:user)
       FactoryGirl.create(:user, name: 'Bob', email: "bob@example.com")
       FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
-      visit user_path
+      visit users_path
     end
 
     it {should have_title('All user')}
     it {should have_content('All user')}
 
-    it "should list each user" do
-      User.all.each do |user|
-        expect(page).to have_selector('li', text: user.name)
+    describe "pagination" do
+      before(:all) {30.times {FactoryGirl.create(:user)}}
+      after(:all) {User.delete_all}
+
+      it {should have_selector('div.pagination')}
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
     end
   end
 
