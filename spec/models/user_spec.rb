c12e4c8e2bb35995d02d9406cd469052ee8c39ba
Greifrut <artur.bunko@gmail.com>
2018-02-21 16:00:51 +0300

    confirm Users db

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,10 @@ describe User do
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
   it {should respond_to(:authenticate)}
+  it {should respond_to(:admin)}
+
   it {should be_valid}
+  it {should_not be_admin}
 
   describe "when name is not present" do
     before { @user.name = "a" * 51}
@@ -92,4 +95,11 @@ describe User do
     end
   end
 
+  describe "with admin attribute set to 'true' " do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    it {should be_admin}
+  end
 end
