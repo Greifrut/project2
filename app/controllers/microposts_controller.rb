9b43859502cc114f6affd3640b323e52db21647d
Greifrut <artur.bunko@gmail.com>
2018-02-24 17:28:57 +0300

    end commit;

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,10 +2,27 @@ class MicropostsController < ApplicationController
 	before_action :signed_in_user
 
 	def create
-	
+		@micropost = curent_user.microposts.build(micropost_params)
+
+		if @micropost.save
+			flash[:succes] = "Micropost created!"
+			redirect_to root_path
+		else
+			render 'static_pages/home'
+		end
+
 	end
 
 	def destroy
 	
 	end
+
+
+	private
+
+		def micropost_params
+			params.require(:micropost).permit(:contet)
+		end
+
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+  	@micropost = curent_user.microposts.build if signed_in?
   end
 
   def help
